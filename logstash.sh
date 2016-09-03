858321b0b359bfd1b3c28739d649cf1af99a1995
Michael Jenkins <michael@managedkaos.com>
2016-09-03 08:01:54 -0700

    Create logstash install script for CentOS.

---
+++ logstash.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+cat << EOF > /etc/yum.repos.d/logstash.repo
+
+[logstash-2.2]
+name=logstash repository for 2.2 packages
+baseurl=http://packages.elasticsearch.org/logstash/2.2/centos
+gpgcheck=1
+gpgkey=http://packages.elasticsearch.org/GPG-KEY-elasticsearch
+enabled=1
+EOF
+
+sudo yum -y install logstash
